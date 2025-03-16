56b11b9ee7e25c089fc3e301faf12f36feeb0b57
Alok-Kumar2005 <ay747283@gmail.com>
2025-03-16 13:36:30 +0530

    model registry and bug fixed

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -4,8 +4,6 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 import yaml
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from src.logger import logging
 
 
---
+++ src/model/register_model.py
@@ -0,0 +1,65 @@
+import json
+import mlflow
+import logging
+from src.logger import logging
+import os
+import dagshub
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.filterwarnings("ignore")
+
+
+
+mlflow.set_tracking_uri('https://dagshub.com/ay747283/Chrome-Plugin.mlflow')
+dagshub.init(repo_owner='ay747283', repo_name='Chrome-Plugin', mlflow=True)
+
+
+def load_model_info(file_path: str) -> dict:
+    """Load the model info from a JSON file."""
+    try:
+        with open(file_path, 'r') as file:
+            model_info = json.load(file)
+        logging.debug('Model info loaded from %s', file_path)
+        return model_info
+    except FileNotFoundError:
+        logging.error('File not found: %s', file_path)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the model info: %s', e)
+        raise
+
+def register_model(model_name: str, model_info: dict):
+    """Register the model to the MLflow Model Registry."""
+    try:
+        model_uri = f"runs:/{model_info['run_id']}/{model_info['model_path']}"
+        
+        # Register the model
+        model_version = mlflow.register_model(model_uri, model_name)
+        
+        # Transition the model to "Staging" stage
+        client = mlflow.tracking.MlflowClient()
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage="Staging"  ## keeping model to staging
+        )
+        
+        logging.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
+    except Exception as e:
+        logging.error('Error during model registration: %s', e)
+        raise
+
+def main():
+    try:
+        model_info_path = 'reports/experiment_info.json'
+        model_info = load_model_info(model_info_path)
+        
+        model_name = "my_model"
+        register_model(model_name, model_info)
+    except Exception as e:
+        logging.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
